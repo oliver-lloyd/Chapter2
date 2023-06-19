093b3b840ab4787808ca436dcbd9f13660169c88
fu19841 <fu19841@bristol.ac.uk>
2023-06-19 15:58:30 +0100

    Including monopharmic side effects

--- data/processed/process_for_libkge.py
+++ data/processed/process_for_libkge.py
@@ -7,7 +7,6 @@ out_edges = pd.DataFrame(columns=['head', 'relation', 'tail'])
 ppi = pd.read_csv('../raw/bio-decagon-ppi.csv')
 ppi['relation'] = 'ProteinProteinInteraction'
 ppi.columns = ['head', 'tail', 'relation']
-
 out_edges = ppi[['head', 'relation', 'tail']]
 del ppi
 
@@ -15,14 +14,19 @@ del ppi
 drug_target = pd.read_csv('../raw/bio-decagon-targets.csv')
 drug_target['relation'] = 'DrugTarget'
 drug_target.columns = ['head', 'tail', 'relation']
-
 out_edges = out_edges.append(drug_target, ignore_index=True)
 del drug_target
+
+# Add monopharmic side effect edges
+monoSE = pd.read_csv('../raw/bio-decagon-mono.csv').drop(columns=['Side Effect Name'])
+monoSE[2] = monoSE.STITCH
+monoSE.columns = ['head', 'relation', 'tail']
+out_edges = out_edges.append(monoSE, ignore_index=True)
+del monoSE
 
 # Add Polypharmic side effect edges
 polySE = pd.read_csv('../raw/bio-decagon-combo.csv').drop(columns=['Side Effect Name'])
 polySE.columns = ['head', 'tail', 'relation']
-
 out_edges = out_edges.append(polySE, ignore_index=True)
 del polySE
 
