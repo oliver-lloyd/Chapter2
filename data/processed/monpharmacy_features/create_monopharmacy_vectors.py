c1e9431928ecab3577376fc67c89699bb4479599
fu19841 <fu19841@bristol.ac.uk>
2023-06-29 16:20:59 +0100

    N-dimensional representations of monopharmacy data

---
+++ data/processed/monpharmacy_features/create_monopharmacy_vectors.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+
+# Load data
+edges = pd.read_csv('../monopharmacy_edges.tsv', header=None, sep='\t').drop(columns=[1])
+edges.columns = ['drug', 'side_effect']
+
+# Create dataframe for full features
+drugs = edges.drug.unique()
+side_effects = edges.side_effect.unique()
+output_shape = (len(drugs), len(side_effects))
+full_features = pd.DataFrame(np.zeros(output_shape), index=drugs, columns=side_effects, dtype=int)
+
+# Populate full features
+for drug, sub_df in edges.groupby('drug'):
+    full_features.loc[drug][sub_df.side_effect] = 1
+
+for n_dim in [32, 64, 128, 256]:
+    pca = PCA(n_components=n_dim)
+    new_matrix = pca.fit_transform(full_features)
+    np.savez_compressed(f'{n_dim}dim.npy', new_matrix)
