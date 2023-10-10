beb8c25e71aad62b946a395e6c785bc2aa37610d
fu19841 <fu19841@bristol.ac.uk>
2023-10-10 10:47:56 +0100

    Separated false edge creation and parallelised it

--- analysis/assessment/assessment.py
+++ analysis/assessment/assessment.py
@@ -9,72 +9,14 @@ from kge.util.io import load_checkpoint
 from sklearn.metrics import roc_auc_score, average_precision_score
 
 
-def create_negative_edges(pos_edges, entity_list):
-    rel = pos_edges[0][1]
-    rel_check = all([edge[1] == rel for edge in pos_edges])
-    if not rel_check:
-        raise ValueError('Positive edges contains multiple relation types.')
-    neg_edges = []
-    while len(neg_edges) < len(pos_edges):
-        head = np.random.choice(entity_list)
-        tail = np.random.choice(entity_list)
-        edge = [head, rel, tail]
-        if edge not in pos_edges and edge not in neg_edges:
-            neg_edges.append(edge)
-    return neg_edges
-
-
 # Get user args
 parser = argparse.ArgumentParser()
-
 parser.add_argument('model_checkpoint')
-parser.add_argument('holdout_edges')
-parser.add_argument('libkge_data_dir')
 parser.add_argument('out_dir')
 parser.add_argument('--partial_results')
-
 args = parser.parse_args()
+
 np.random.seed(0)
-
-# Load data
-entity_ids = pd.read_csv(
-    f'{args.libkge_data_dir}/entity_ids.del',
-    sep='\t',
-    header=None,
-    index_col=0
-).to_dict()[1]
-compound_IDs = [key for key in entity_ids if entity_ids[key].startswith('CID')]
-relation_ids = pd.read_csv(
-    f'{args.libkge_data_dir}/relation_ids.del',
-    sep='\t',
-    header=None,
-    index_col=0
-).to_dict()[1]
-full_edgelist = pd.DataFrame()
-for split in ['train', 'test', 'valid']:
-    full_edgelist = full_edgelist.append(pd.read_csv(
-        f'{args.libkge_data_dir}/{split}.del',
-        sep='\t',
-        header=None
-    ))
-holdout = pd.read_csv(
-    args.holdout_edges,
-    sep='\t',
-    header=None
-)
-
-# Convert holdout edges to IDs if necessary
-if all(holdout.dtypes == object):
-    entity_name_to_id = {entity_ids[key]: key for key in entity_ids}
-    holdout[0] = [entity_name_to_id[name] for name in holdout[0]]
-    holdout[2] = [entity_name_to_id[name] for name in holdout[2]]
-
-    relation_name_to_id = {relation_ids[key]: key for key in relation_ids}
-    holdout[1] = [relation_name_to_id[name] for name in holdout[1]]
-elif any(holdout.dtypes == object):
-    raise ValueError(
-        'Appears that there is a mix of IDs and strings in holdout data.'
-    )
 
 # Load checkpoint
 checkpoint = load_checkpoint(args.model_checkpoint)
@@ -92,33 +34,21 @@ else:
     results = pd.DataFrame(columns=['Relation', 'AUROC', 'AUPRC', 'AP@50'])
 
 # Calculate metrics per relation type
-# TODO: parallelise this, but beware of memory usage on large data
 rel_count = len(holdout[1].unique())
 for rel_id, subdf in holdout.groupby(1):
+
     # Check if already assessed
     relation = relation_ids[rel_id]
     if relation not in results.Relation:
+
         # Get assessment data
         positive_edges = subdf.to_numpy().tolist()
         false_edge_file = f'{rel_id}.tsv'
-        if false_edge_file not in listdir('false_edges'):
-            # Create negative edges if haven't already
-            train_subdf = full_edgelist.loc[full_edgelist[1] == rel_id]
-            negative_edges = create_negative_edges(
-                positive_edges + train_subdf.to_numpy().tolist(),
-                compound_IDs
-            )
-            pd.DataFrame(negative_edges).to_csv(
-                f'false_edges/{false_edge_file}', 
-                header=None, sep='\t', index=False
-            )
-        else:
-            # Otherwise load negative edges
-            negative_edges = pd.read_csv(
-                f'false_edges/{false_edge_file}', 
-                header=None, 
-                sep='\t'
-            ).to_numpy().tolist()
+        negative_edges = pd.read_csv(
+            f'false_edges/{false_edge_file}',
+            header=None,
+            sep='\t'
+        ).to_numpy().tolist()
         edges_to_score = positive_edges + negative_edges
         s = torch.Tensor([edge[0] for edge in edges_to_score])
         p = torch.Tensor([rel_id for edge in edges_to_score])
